812987e68e5c744514979eb615bbf36cf3f82076
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-05-07 16:19:51 -0400

    edited readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Daily-Scheduler
-daily scheduler project for mod 5
+
+## Description
+-a useful application that allows the user to keep their itinerary for themselves handy, as well as pull up the current date and time when needed.
+
+## Table of Contents
+
+- [Usage]
+- [Credits]
+
+## Usage
+
+-Click on any of the time spaces to enter a personal task and save for later to remember!  Note the colored features to see that the date and time is kept for you!
+
+## Credits
+Ryan Mennemeier
+
+## Contact
+Email: kalpha1865@gmail.com
+Github: @Taliwat
--- script.js
+++ script.js
@@ -7,30 +7,33 @@ $(document).ready(function () {
     localStorage.setItem(time, data);
     $('notification').addClass('show');
   });
-
-
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  dayjs().format()
-
-  let now = dayjs();
-  dayjs().toDate();
-  console.log(now.format());
-
-  let currentTime = timeColor 
-  let presentTime = 
-
-  function checkEqual(now, currentTime) {
-    return currentTime > now ? "grey"
-        : now = currentTime ? "red"
-        : "green"
+  
+  
+  
+  //get current time from Dayjs, and code color for current time of day.
+  function timeStatus() {
+    const currentTime = dayjs().hour();
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+      
+      if (blockHour < currentTime) {
+        $(this).addClass('past');
+      } else if (blockHour === currentTime) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
   }
-
+  timeStatus();
+  
+  
+  // show current date on top of page
+  $('currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+  
   //show or load the localStorage data from previous use
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
@@ -41,10 +44,6 @@ $(document).ready(function () {
   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-  
-  // show current date on top of page
-  $('currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
-
 });
 
 
